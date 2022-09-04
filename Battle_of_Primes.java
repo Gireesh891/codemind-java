6ae56297a70b2b2201069e1a828d3e92d70ab9cc
Gireesh891 <84901927+Gireesh891@users.noreply.github.com>
2022-09-04 10:36:37 +0530

    import java.util.Scanner; class java {     public static  boolean isprime(int n)     {         int count=0;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             count++;         }         if(count==0)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,c=0,count=0;         a=sc.nextInt();         b=sc.nextInt();         c=a+b;         for(int i=c+1;;i++)         {             count++;             if(isprime(i))             break;         }         System.out.println(count);
        }
    }

---
+++ Battle_of_Primes.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class java
+{
+    public static  boolean isprime(int n)
+    {
+        int count=0;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            count++;
+        }
+        if(count==0)
+        return true;
+        else
+        return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c=0,count=0;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=a+b;
+        for(int i=c+1;;i++)
+        {
+            count++;
+            if(isprime(i))
+            break;
+        }
+        System.out.println(count);
+        
+    }
+}
