fd622a691e8d670cc58656a84a87e30a0e51eb1a
Gireesh891 <84901927+Gireesh891@users.noreply.github.com>
2022-09-11 08:41:20 +0530

    import java.util.Scanner; class java {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         char ch=sc.nextLine().charAt(0);         int k=ch;         System.out.print(k);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class java
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.nextLine().charAt(0);
+        int k=ch;
+        System.out.print(k);
+    }
+}
