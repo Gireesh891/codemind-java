57766ad145146009192f876997d3600c17e6461b
Gireesh891 <84901927+Gireesh891@users.noreply.github.com>
2022-10-01 10:30:17 +0530

    import java.util.Scanner; class java {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,sum=0,r;         n=sc.nextInt();         while(n!=0)         {             r=n%10;             sum=sum*10+r;             n=n/10;
            }
            System.out.println(sum);
        }
    }

--- Reverse_Integer.java
+++ Reverse_Integer.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 class java
 {
-    public static  void main(String args[])
+    public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int r,sum=0,n;
+        int n,sum=0,r;
         n=sc.nextInt();
         while(n!=0)
         {
             r=n%10;
             sum=sum*10+r;
             n=n/10;
+            
         }
         System.out.println(sum);
-        
     }
 }
