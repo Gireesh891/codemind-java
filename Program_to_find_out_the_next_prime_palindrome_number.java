371b508899e4972611c10c231606faed1318599e
Gireesh891 <84901927+Gireesh891@users.noreply.github.com>
2022-10-04 17:55:00 +0530

    import java.util.Scanner; class java {     public static boolean isprime(int n)     {         int count=0;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             count++;         }         if(count==0)         return true;         else         return false;     }     public static  boolean ispalindrome(int m)     {         int  r,sum=0;         int temp=m;         while(m>0)         {             r=m%10;             sum=sum*10+r;             m=m/10;         }         if(sum==temp)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a;         a=sc.nextInt();         for(int i=a+1;;i++)         {             if(isprime(i) && ispalindrome(i))             {                 System.out.println(i);                 break;             }         }     } }

--- Program_to_find_out_the_next_prime_palindrome_number.java
+++ Program_to_find_out_the_next_prime_palindrome_number.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 class java
 {
-    public static boolean isprime(int a)
+    public static boolean isprime(int n)
     {
         int count=0;
-        for(int i=2;i<=(int)Math.sqrt(a);i++)
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
         {
-            if(a%i==0)
+            if(n%i==0)
             count++;
         }
         if(count==0)
@@ -14,9 +14,9 @@ class java
         else
         return false;
     }
-    public static boolean ispalindrome(int m)
+    public static  boolean ispalindrome(int m)
     {
-        int r,sum=0;
+        int  r,sum=0;
         int temp=m;
         while(m>0)
         {
@@ -32,9 +32,9 @@ class java
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n;
-        n=sc.nextInt();
-        for(int i=n+1;;i++)
+        int a;
+        a=sc.nextInt();
+        for(int i=a+1;;i++)
         {
             if(isprime(i) && ispalindrome(i))
             {
