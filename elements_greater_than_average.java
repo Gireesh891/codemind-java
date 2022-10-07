fcc9fa3725f5bf7d49b1dfaf27ae7fdaa625f429
Gireesh891 <84901927+Gireesh891@users.noreply.github.com>
2022-10-07 15:23:26 +0530

    import java.util.Scanner; class java {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n,avg=0,sum=0,count=0;         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {         x[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             sum=sum+x[i];         }         avg=sum/n;         for(int i=0;i<n;i++)         {         if(avg<=x[i])         {         count++;         }         }         System.out.println(count);
        }
    }

--- elements_greater_than_average.java
+++ elements_greater_than_average.java
@@ -4,25 +4,26 @@ class java
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int x[],n,i,count=0,sum=0,k=0;
+        int x[],n,avg=0,sum=0,count=0;
         n=sc.nextInt();
         x=new int[n];
-        for(i=0;i<n;i++)
+        for(int i=0;i<n;i++)
+        {
         x[i]=sc.nextInt();
-        for(i=0;i<n;i++)
+        }
+        for(int i=0;i<n;i++)
         {
             sum=sum+x[i];
         }
-        k=sum/n;
-        for(i=0;i<n;i++)
+        avg=sum/n;
+        for(int i=0;i<n;i++)
         {
-            if(x[i]>=k)
-            {
-                count++;
-            }
+        if(avg<=x[i])
+        {
+        count++;
+        }
         }
         System.out.println(count);
-        
-        
+           
     }
 }
