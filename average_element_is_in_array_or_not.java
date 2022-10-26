0e09287670bbedad9535fca164c2179d085023b7
Gireesh891 <84901927+Gireesh891@users.noreply.github.com>
2022-10-26 21:21:26 +0530

    import java.util.Scanner; class java {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],sum=0,found=0,avg=0;         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             sum=sum+x[i];         }         avg=(int)sum/n;         for(int i=0;i<n;i++)         {             if(x[i]==avg)             {                 found=1;             }         }         if(found==1)         {             System.out.println("True");         }         else         {             System.out.println("False");         }     } }

--- average_element_is_in_array_or_not.java
+++ average_element_is_in_array_or_not.java
@@ -4,8 +4,7 @@ class java
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],found=0,sum=0;
-        float avg=0;
+        int n,x[],sum=0,found=0,avg=0;
         n=sc.nextInt();
         x=new int[n];
         for(int i=0;i<n;i++)
@@ -21,8 +20,7 @@ class java
         {
             if(x[i]==avg)
             {
-               found=1;
-                
+                found=1;
             }
         }
         if(found==1)
@@ -33,6 +31,5 @@ class java
         {
             System.out.println("False");
         }
-           
     }
 }
