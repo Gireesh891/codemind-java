cd25c8cfe54d457075a83e1c2eb7cc64db025a94
Gireesh891 <84901927+Gireesh891@users.noreply.github.com>
2022-10-26 21:15:26 +0530

    import java.util.Scanner; class java {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],found=0;         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         int a=sc.nextInt();         for(int i=0;i<n;i++)         {             if(x[i]==a)             {                 found=1;             }         }         if(found==1)         {         System.out.println("True");         }         else         {             System.out.println("False");         }     } }

--- element_search_in_an_array.java
+++ element_search_in_an_array.java
@@ -4,29 +4,28 @@ class java
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],a,found=0;
+        int n,x[],found=0;
         n=sc.nextInt();
         x=new int[n];
         for(int i=0;i<n;i++)
         {
             x[i]=sc.nextInt();
         }
-        a=sc.nextInt();
+        int a=sc.nextInt();
         for(int i=0;i<n;i++)
         {
-           if(x[i]==a)
-           {
-               found=1;
-           }
+            if(x[i]==a)
+            {
+                found=1;
+            }
         }
         if(found==1)
         {
-            System.out.println("True");
+        System.out.println("True");
         }
         else
         {
             System.out.println("False");
         }
-           
     }
 }
