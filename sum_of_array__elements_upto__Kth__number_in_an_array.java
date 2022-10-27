b80664d1e93d98d67c1b9cfcd851d15c5c35314f
Gireesh891 <84901927+Gireesh891@users.noreply.github.com>
2022-10-27 20:52:06 +0530

    import java.util.Scanner; class java {     public static void main(String arga[])     {         Scanner sc=new Scanner(System.in);         int n,x[],k,sum=0;         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         k=sc.nextInt();         for(int i=0;i<k;i++)         {             sum=sum+x[i];
            }
            System.out.println(sum);
            
        }
    }

--- sum_of_array__elements_upto__Kth__number_in_an_array.java
+++ sum_of_array__elements_upto__Kth__number_in_an_array.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
 class java
 {
-    public static void main(String args[])
+    public static void main(String arga[])
     {
         Scanner sc=new Scanner(System.in);
-        int x[],n,i,sum=0,k=0;
+        int n,x[],k,sum=0;
         n=sc.nextInt();
         x=new int[n];
-        for(i=0;i<n;i++)
-        x[i]=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
         k=sc.nextInt();
-        for(i=0;i<k;i++)
+        for(int i=0;i<k;i++)
         {
             sum=sum+x[i];
+            
         }
         System.out.println(sum);
+        
     }
 }
