d70d70e94b51dc3710b85c809e410d709d827ba3
Gireesh891 <84901927+Gireesh891@users.noreply.github.com>
2022-10-27 06:49:17 +0530

    import java.util.Scanner; class java {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],sum=0;         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             if(x[i]!=-1)             {                 for(int j=0;j<n;j++)                 {                     if(x[i]==x[j] && i!=j)                     {                         x[j]=-1;                     }                 }                 sum=sum+x[i];             }         }         System.out.println(sum);
        }
    }

---
+++ unique_sum.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class java
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],sum=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(x[i]!=-1)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(x[i]==x[j] && i!=j)
+                    {
+                        x[j]=-1;
+                    }
+                }
+                sum=sum+x[i];
+            }
+        }
+        System.out.println(sum);
+        
+    }
+}
