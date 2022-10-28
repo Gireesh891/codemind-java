23920cbbe47034a8d886edcc33831ce45cbdcfc1
Gireesh891 <84901927+Gireesh891@users.noreply.github.com>
2022-10-28 06:17:48 +0530

    import java.util.Scanner; class java {     public static boolean ispalindrome(int a)     {         int rev=0,sum=0,temp=a;         while(a>0)         {             rev=a%10;             sum=sum*10+rev;             a=a/10;         }         if(sum==temp)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],count=0;         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {            if(ispalindrome(x[i]))            {                count++;            }         }         System.out.println(count);
        }
    }

--- Count_palindromes.java
+++ Count_palindromes.java
@@ -1,35 +1,39 @@
 import java.util.Scanner;
 class java
 {
-    public static boolean ispalindrome(int n)
+    public static boolean ispalindrome(int a)
     {
-        int sum=0,r,temp=0;
-        temp=n;
-        while(n>0)
+        int rev=0,sum=0,temp=a;
+        while(a>0)
         {
-            r=n%10;
-            sum=sum*10+r;
-            n=n/10;
+            rev=a%10;
+            sum=sum*10+rev;
+            a=a/10;
         }
         if(sum==temp)
         return true;
         else
-        return false;
+        return false; 
     }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int x[],a,i,count=0;
-        a=sc.nextInt();
-        x=new int[a];
-        for(i=0;i<a;i++)
-        x[i]=sc.nextInt();
-        for(i=0;i<a;i++)
+        int n,x[],count=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
         {
-            if(ispalindrome(x[i]))
-            count++;
-            
+            x[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+           if(ispalindrome(x[i]))
+           {
+               count++;
+           }
         }
         System.out.println(count);
+        
+        
     }
 }
