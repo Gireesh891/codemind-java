9680730a7ca6fd1f438dbfa6883a72959c399bb5
Gireesh891 <84901927+Gireesh891@users.noreply.github.com>
2022-10-30 14:43:02 +0530

    import java.util.Scanner; import java.util.Arrays; class java {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[];         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(int i=n-1;i>=n/2;i--)         {             System.out.print(x[i]+" ");         }         for(int i=0;i<n/2;i++)         {             System.out.print(x[i]+" ");         }
        }
    }

---
+++ Half_List.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class java
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[];
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(int i=n-1;i>=n/2;i--)
+        {
+            System.out.print(x[i]+" ");
+        }
+        for(int i=0;i<n/2;i++)
+        {
+            System.out.print(x[i]+" ");
+        }
+        
+        
+        
+    }
+}
