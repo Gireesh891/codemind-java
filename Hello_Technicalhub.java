35053c97173051afc9c772412f06f73763f68281
Gireesh891 <84901927+Gireesh891@users.noreply.github.com>
2022-11-06 16:08:22 +0530

    import java.util.Scanner; class java {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s1=sc.nextLine();         System.out.println("Hello Technicalhub");         System.out.println(s1);
        }
    }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class java
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        System.out.println("Hello Technicalhub");
+        System.out.println(s1);
+        
+        
+    }
+}
