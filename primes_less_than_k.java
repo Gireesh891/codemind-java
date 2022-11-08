ed1b41e8b86757377fbd9b655207cea2dc378748
Gireesh891 <84901927+Gireesh891@users.noreply.github.com>
2022-11-08 15:42:34 +0530

    import java.util.Scanner; class java {     public static boolean isprime(int a)     {         int count=0;         for(int i=1;i<=a;i++)         {             if(a%i==0)             {                 count++;             }         }         if(count==2)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],c=0;         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         int k=sc.nextInt();         for(int i=0;i<n;i++)         {             if(isprime(x[i]))             {                 if(x[i]<=k)                 {                     c++;                 }
                }
            }
            System.out.println(c);
            
            
        }
    }

--- primes_less_than_k.java
+++ primes_less_than_k.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 class java
 {
-    public static boolean isprime(int n)
+    public static boolean isprime(int a)
     {
         int count=0;
-        if(n<2)
-        return false;
-        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        for(int i=1;i<=a;i++)
         {
-            if(n%i==0)
-            count++;
+            if(a%i==0)
+            {
+                count++;
+            }
         }
-        if(count==0)
+        if(count==2)
         return true;
         else
         return false;
@@ -19,20 +19,27 @@ class java
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int x[],a,i,count=0,k;
-        a=sc.nextInt();
-        x=new int[a];
-        for(i=0;i<a;i++)
-        x[i]=sc.nextInt();
-        k=sc.nextInt();
-        for(i=0;i<a;i++)
+        int n,x[],c=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
+        for(int i=0;i<n;i++)
         {
             if(isprime(x[i]))
             {
                 if(x[i]<=k)
-                count++;
+                {
+                    c++;
+                }
+               
             }
         }
-        System.out.println(count);
+        System.out.println(c);
+        
+        
     }
 }
