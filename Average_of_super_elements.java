a14981e6252fbe4d901ceb64a83d811eb720d082
Gireesh891 <84901927+Gireesh891@users.noreply.github.com>
2023-01-22 14:37:12 +0530

    import java.util.*; import java.util.Map; class Sample {     public static float printoutput(int arr[])     {         int sum=0,count=0;         HashMap<Integer,Integer> hm=new HashMap<>();         for(int i=0;i<arr.length;i++)         {             hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);         }         for(Map.Entry entry:hm.entrySet())         {             if((int)entry.getKey()==(int)entry.getValue())             {                 sum=sum+(int)entry.getKey();                 count++;             }         }         if(count==0)         return -1;         else         return (float)sum/count;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[];         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         float result=printoutput(x);         if(result<0)         {             System.out.println("-1");         }         else         {             System.out.format("%.2f",result);         }     } }

---
+++ Average_of_super_elements.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.util.Map;
+class Sample
+{
+    public static float printoutput(int arr[])
+    {
+        int sum=0,count=0;
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+        }
+        for(Map.Entry entry:hm.entrySet())
+        {
+            if((int)entry.getKey()==(int)entry.getValue())
+            {
+                sum=sum+(int)entry.getKey();
+                count++;
+            }
+        }
+        if(count==0)
+        return -1;
+        else
+        return (float)sum/count;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[];
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        float result=printoutput(x);
+        if(result<0)
+        {
+            System.out.println("-1");
+        }
+        else
+        {
+            System.out.format("%.2f",result);
+        }
+    }
+}
